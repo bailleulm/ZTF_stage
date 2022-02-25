c1e027ed6f3d2fdc785af9072b13042e210d40d0
Manon <manon30270@gmail.com>
2022-02-25 15:48:56 +0100

    notebook modif with sncosmo fit

--- ztf_simu/ZTF_hdf5.py
+++ ztf_simu/ZTF_hdf5.py
@@ -127,7 +127,7 @@ class Read_LightCurve:
             Returns the reading of an .hdf5 file as an AstropyTable.
         """
         
-        return astropy.io.misc.hdf5.read_table_hdf5(self.file, path=path)
+        return astropy.io.misc.hdf5.read_table_hdf5(self.file, path=path, character_as_bytes=False)
 
 
 # In[257]:
