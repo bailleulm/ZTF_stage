9c2283fa05d34c39bf85b53e962897c2d1fe73e5
Philippe Gris <gris@clermont.in2p3.fr>
2022-03-11 17:05:48 +0100

    implement multiprocessing

--- script/run_fit_lc.py
+++ script/run_fit_lc.py
@@ -2,6 +2,20 @@ from optparse import OptionParser
 from ztf_fit import SN_fit_tab
 from ztf_hdf5 import Read_LightCurve
 import astropy
+from ztf_util import multiproc
+
+
+def fit(metaTable, params, j=0, output_q=None):
+
+    sn_fit = SN_fit_tab(metaTable)
+
+    resfit = sn_fit()
+
+    if output_q is not None:
+        return output_q.put({j: resfit})
+    else:
+        return 0
+
 
 parser = OptionParser()
 
@@ -13,20 +27,25 @@ parser.add_option('--metaFileOutput', type=str, default='Meta_fit.hdf5',
                   help='meta file name for output [%default]')
 parser.add_option('--metaDirOutput', type=str, default='dataLC',
                   help='meta dir for output [%default]')
+parser.add_option('--nproc', type=int, default=1,
+                  help='number of procs for multiprocessing [%default]')
+
 opts, args = parser.parse_args()
 
 metaFileInput = opts.metaFileInput
 metaDirInput = opts.metaDirInput
 metaFileOutput = opts.metaFileOutput
 metaDirOutput = opts.metaDirOutput
+nproc = opts.nproc
 
 meta = Read_LightCurve(file_name=metaFileInput, inputDir=metaDirInput)
 metaTable = meta.get_table(path='meta')
 
-sn_fit = SN_fit_tab(metaTable)
+params = {}
+resfit = multiproc(metaTable, params, fit, nproc)
 
-resfit = sn_fit()
 print(len(metaTable), len(resfit))
+
 # write results in file
 fOut = '{}/{}'.format(metaDirOutput, metaFileOutput)
 astropy.io.misc.hdf5.write_table_hdf5(
