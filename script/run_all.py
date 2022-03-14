67c8372702c0701e67323186a9a2a5a570deba86
Manon <manon30270@gmail.com>
2022-03-14 20:13:45 +0100

    add modif

--- script/run_all.py
+++ script/run_all.py
@@ -1,7 +1,19 @@
 import os
+from optparse import OptionParser
 
-ntransient = 88
-nproc = 8
+parser = OptionParser()
+parser.add_option('--nproc', type=int, default=1,
+                  help='number of procs for multiprocessing [%default]')
+parser.add_option('--ntransient', type=int, default=11,
+                  help='number of transientor [%default]')
+parser.add_option('--zmin', type=int, default=0.01,
+                  help='zmin [%default]')
+parser.add_option('--zmax', type=int, default=0.1,
+                  help='zmax [%default]')
+
+opts, args = parser.parse_args()
+ntransient = opts.ntransient
+nproc = opts.nproc
 
 # simulation
 simu_script = 'ztf_stage/script/run_simulation.py'
@@ -16,5 +28,5 @@ os.system(info_cmd)
 
 # fit
 fit_script = 'ztf_stage/script/run_fit_lc.py'
-fit_cmd = 'python {} --nproc {}'.format(fit_script, ntransient, nproc)
+fit_cmd = 'python {} --nproc {}'.format(fit_script, nproc)
 os.system(fit_cmd)
